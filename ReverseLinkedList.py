973968e82fd121fc2515a652cf0dc563dcee5486
alan <rlalan@outlook.com>
2018-03-31 11:49:37 +0800

    reverse linked list

---
+++ ReverseLinkedList.py
@@ -0,0 +1,30 @@
+# 链表反转（递归）
+
+
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverse(head, newhead):
+    tempnext = head.next
+    head.next = newhead
+    newhead = tempnext
+    if newhead == None:
+        return
+    reverse(newhead, head)
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+reverse(n1, Node)
+print(n4.next.value)
+print(n3.next.value)
+print(n2.next.value)
+print(n1.next)
