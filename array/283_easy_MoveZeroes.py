0bf08dd945ae9570cb249e821e0f7605b6e5d1cf
alan <rlalan@outlook.com>
2018-04-15 17:51:24 +0800

    update 283

--- array/283_easy_MoveZeroes.py
+++ array/283_easy_MoveZeroes.py
@@ -9,6 +9,8 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        if len(nums) == 0:
+            return
         self.trs(nums)
         return nums
 
@@ -22,5 +24,47 @@ class Solution:
             return
 
 
+class Solution2:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 0:
+            return
+        self.trs(0, nums)
+
+    def trs(self, begin, nums):
+        for i in range(begin, len(nums)):
+            if nums[i] == 0 and i + 1 != len(nums):
+                if nums[i + 1] == 0:
+                    self.trs(i + 1, nums)
+                temp = nums[i + 1]
+                nums[i + 1] = nums[i]
+                nums[i] = temp
+            print(nums)
+
+
+# class Solution3:
+#     def moveZeroes(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: void Do not return anything, modify nums in-place instead.
+#         """
+#         print(nums)
+#         if len(nums) == 0:
+#             return
+#         j = len(nums) - 1
+#         for i in range(len(nums) - 1, 1, -1):
+#             for l in range(i, j + 1):
+#                 if l + 1 == len(nums):
+#                     break
+#                 temp = nums[l]
+#                 nums[l] = nums[l + 1]
+#                 nums[l + 1] = temp
+#             j = j - 1
+#         print(nums)
+
+
 nums = [0, 1, 0, 3, 12]
-print(Solution().moveZeroes(nums))
+print(Solution2().moveZeroes(nums))
