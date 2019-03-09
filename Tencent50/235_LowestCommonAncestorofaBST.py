d12cc7f356f8a3a54d588af9842e06c5c4ec8c3e
AlanConstantine <rlalan@outlook.com>
2019-03-09 20:44:36 +0800

    Update 235

--- Tencent50/235_LowestCommonAncestorofaBST.py
+++ Tencent50/235_LowestCommonAncestorofaBST.py
@@ -33,8 +33,24 @@
 #         self.left = None
 #         self.right = None
 
-
 class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
+
+
+class SolutionII(object):
     def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
