5b2af75feed991765fd1f0bd018cf4c02eef5993
AlanConstantine <rlalan@outlook.com>
2019-03-11 17:47:00 +0800

    Update 236

--- Tree/236_medium_LowestCommonAncestorofaBinaryTree.py
+++ Tree/236_medium_LowestCommonAncestorofaBinaryTree.py
@@ -49,15 +49,20 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        # 递归思想, 对以root为根的(子)树进行查找p和q, 如果root == null || p || q 直接返回root
         if root == None or root == p or root == q:
             return root
+        # 表示对于当前树的查找已经完毕, 否则对左右子树进行查找, 根据左右子树的返回值判断:
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
         if left == None and right == None:
+            # 1. 左右子树返回值均为null, p和q均不在树中, 返回null
             return None
         elif left != None and right != None:
+            # 2. 左右子树的返回值都不为null, 由于值唯一左右子树的返回值就是p和q, 此时root为LCA
             return root
         else:
+            # 3. 如果左右子树返回值只有一个不为null, 说明只有p和q存在与左或右子树中, 最先找到的那个节点为LCA
             return right if left == None else left
 
 
