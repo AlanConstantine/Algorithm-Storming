fa2477c3fa572a446a1eb961ec72519dc5d138e0
AlanConstantine <rlalan@outlook.com>
2019-09-12 22:49:26 +0800

    Update 182

--- Leetcode/Sql/182_easy_DuplicateEmails.sql
+++ Leetcode/Sql/182_easy_DuplicateEmails.sql
@@ -1,3 +1,8 @@
 -- SQLite
-SELECT Email, count(Email) as num FROM "182_Person";
+
+SELECT Email FROM (
+SELECT Email, count(Email) as num 
+FROM "182_Person" GROUP BY Email
+) AS statistic
+WHERE num > 1;
 -- UPDATE "182_Person" SET Email = "c@d.com" WHERE Id = 2;
