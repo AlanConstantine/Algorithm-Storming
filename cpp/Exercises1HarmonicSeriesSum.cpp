7c0441418c7074253cc06ce8ee7145a169a38986
LIU Ruilun <rlalan@outlook.com>
2020-07-28 10:57:50 +0800

    Update Exercise1HarmonicSeriesSum

---
+++ cpp/Exercises1HarmonicSeriesSum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout << "Please enter n: ";
+    cin >> n;
+    cout << "Input n: " << n << endl;
+    double harmonicSum = 0.0;
+    int i = 1;
+    while (i <= n){
+        harmonicSum = harmonicSum + double(1.0/i);
+        ++i;
+        // cout << "test: " << harmonicSum << endl;
+    }
+    cout << "The Harmonic sum is " << harmonicSum << endl;
+}
