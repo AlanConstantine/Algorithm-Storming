61fbb53e8a9c7211ead3e72d27488971ec2cc592
AlanConstantine <rlalan@outlook.com>
2020-07-30 15:52:34 +0800

    Update Exercise Time

---
+++ cpp/Exercises3/Time/Time.h
@@ -0,0 +1,23 @@
+
+class Time {
+    private:
+        int hour;
+        int minute;
+        int second;
+    public:
+        Time(int hour, int minute, int second) {
+            this->hour = hour;
+            this->minute = minute;
+            this->second = second;
+        }
+
+        int getHour() const;
+        int getMinute() const;
+        int getSecond() const;
+        void setHour(int hour);
+        void setMinute(int minute);
+        void setSecond(int second);
+        void setTime(int hour, int minute, int second);
+        void print() const;
+        void nextSecond();
+};
