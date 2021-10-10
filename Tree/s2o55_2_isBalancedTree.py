4910674fa3c762ffd691e71e9353347e8b2d0707
AlanConstantine <rlalan@outlook.com>
2021-10-10 14:00:39 +0800

    Update s2o55_2

--- Tree/s2o55_2_isBalancedTree.py
+++ Tree/s2o55_2_isBalancedTree.py
@@ -1,5 +1,19 @@
 from Tree import TreeNode, pre_traversal
 
+
+def isbalancedtree(root):
+    if not root:
+        return 0, True
+
+    leftd, unb = isbalancedtree(root.left)
+    rightd, unb = isbalancedtree(root.right)
+    if abs(leftd-rightd) > 1:
+        unb = False
+    else:
+        unb = True
+    return leftd+1 if leftd > rightd else rightd+1, unb
+
+
 a = TreeNode(1)
 b = TreeNode(2)
 c = TreeNode(3)
@@ -7,6 +21,8 @@ d = TreeNode(4)
 e = TreeNode(5)
 f = TreeNode(6)
 g = TreeNode(7)
+h = TreeNode(8)
+i = TreeNode(9)
 
 a.left = b
 a.right = c
@@ -14,5 +30,8 @@ b.left = d
 b.right = e
 e.left = g
 c.right = f
+g.left = h
+f.right = i
 
 print(pre_traversal(a))
+print(isbalancedtree(a)[1])
