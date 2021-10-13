1dda2ed418e2783030aeabc1329e0bd1369d5312
AlanConstantine <rlalan@outlook.com>
2021-10-13 23:10:22 +0800

    Update leetcode 95

---
+++ Leetcode/Tree/95_generateTrees.py
@@ -0,0 +1,14 @@
+# 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。
+# 可以按 任意顺序 返回答案。
+
+class TreeNode(object):
+    def __init__(self, val):
+        super().__init__()
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def generateTrees(self, n):
+        pass
