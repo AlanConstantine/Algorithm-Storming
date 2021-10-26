b7824633d0ca54502d5ce2ab53846aa8f5ea291c
AlanConstantine <rlalan@outlook.com>
2021-10-27 01:28:22 +0800

    Update 108

---
+++ Leetcode/Tree/108_easy_ConvertSortedArraytoBinarySearchTree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def in_traversal(root):
+    a = []
+    if root:
+        a += in_traversal(root.left)
+        a.append(root.val)
+        a += in_traversal(root.right)
+    return a
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+        if len(nums) == 1:
+            root = TreeNode(nums[0])
+            return root
+        if len(nums) == 2:
+            root = TreeNode(nums[1])
+            root.left = TreeNode(nums[0])
+            return root
+        if len(nums) > 2:
+            mid = len(nums)//2
+            root = TreeNode(nums[mid])
+            leftnums = nums[:mid]
+            rightnums = nums[mid+1:]
+            root.left = self.sortedArrayToBST(leftnums)
+            root.right = self.sortedArrayToBST(rightnums)
+            return root
+
+
+nums = [-10, -3, 0, 5, 9]
+S = Solution()
+print(in_traversal(S.sortedArrayToBST(nums)))
