d095c4d0594c6bfef78ddb9e3bb225de08a20275
AlanConstantine <rlalan@outlook.com>
2021-10-30 16:54:17 +0800

    update 99

--- Leetcode/Tree/99_medium_RecoverBinarySearchTree.py
+++ Leetcode/Tree/99_medium_RecoverBinarySearchTree.py
@@ -4,6 +4,17 @@
 
 # Definition for a binary tree node.
 
+# 给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+
+# 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/recover-binary-search-tree
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# 中序遍历，判断是否升序，用一个指针pre记录前一个节点，然后判断pre和当前节点的大小来判断是否升序
+# 两种顺序错误情况：第一种两个错误的位置相邻（1，3，2，4），第二种两个错误的位置不相邻（1，4，3，2）
+
 def in_traversal(root):
     s = []
     if root:
