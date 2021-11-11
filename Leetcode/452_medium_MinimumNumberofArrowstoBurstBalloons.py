0bff9e0c640e748c1da0e1a3ca01a0d56235d950
AlanConstantine <rlalan@outlook.com>
2021-11-11 12:40:13 +0800

    update 452

--- Leetcode/452_medium_MinimumNumberofArrowstoBurstBalloons.py
+++ Leetcode/452_medium_MinimumNumberofArrowstoBurstBalloons.py
@@ -1,9 +1,7 @@
 class Solution:
     def findMinArrowShots(self, points):
-        if not points:
-            return 0
-        if len(points) == 1:
-            return 1
+        if len(points) <= 1:
+            return len(points)
         res = 1
         points.sort(key=lambda x: x[1])
         end = points[0][-1]
