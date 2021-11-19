641a66543edca6a5db4b4411a3b0e9b09ec05f25
AlanLau <rlalan@outlook.com>
2021-11-19 14:20:08 +0800

    Update s2oII052

--- Tree/s2oII052increasingBST.py
+++ Tree/s2oII052increasingBST.py
@@ -1,7 +1,7 @@
 # https://leetcode-cn.com/problems/NYBBNL/
 # 给你一棵二叉搜索树，请 按中序遍历 将其重新排列为一棵递增顺序搜索树，
 # 使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
-
+# same as: https://leetcode-cn.com/problems/increasing-order-search-tree/
 
 # Definition for a binary tree node.
 class TreeNode:
