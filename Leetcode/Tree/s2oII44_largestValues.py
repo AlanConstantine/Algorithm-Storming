ccea306ce4ba1ce04e54e9e16a6054dce2f5757d
AlanConstantine <rlalan@outlook.com>
2021-11-30 14:58:59 +0800

    add s2oII44

---
+++ Leetcode/Tree/s2oII44_largestValues.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        ans = []
+        s = [root]
+        while s:
+            ans.append(max([n.val for n in s]))  # 不一定用遍历，可以用一个最小值来比较
+            stmp = []
+            for n in s:
+                if n.left:
+                    stmp.append(n.left)
+                if n.right:
+                    stmp.append(n.right)
+            s = stmp
+        return ans
