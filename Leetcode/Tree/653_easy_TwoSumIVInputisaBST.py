4e8ee9958a70956849c343297b6d1627ed50869b
AlanConstantine <rlalan@outlook.com>
2021-12-08 18:58:30 +0800

    unfinished s2oII50

---
+++ Leetcode/Tree/653_easy_TwoSumIVInputisaBST.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Author: Alan Lau
+# @Date: 2021-12-08 18:48:31
+
+# 给定一个二叉搜索树 root 和一个目标结果 k，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+# https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        if not root:
+            return None
+        sub = []
+        self.ans = False
+
+        def dfs(root):
+            if not root:
+                return None
+            dfs(root.left)
+            if root.val in sub:
+                self.ans = True
+                return
+            sub.append(k - root.val)
+            dfs(root.right)
+
+        dfs(root)
+        return self.ans
---
+++ Leetcode/Tree/Ns2oII50_medium_pathSum.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Alan Lau
+# @Date: 2021-12-08 15:47:16
+
+# 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+
+# 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/6eUYwP
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> int:
+        pass
