275b5ceb9f7ad8c90ae7f03243c4ea22554db799
AlanConstantine <rlalan@outlook.com>
2022-01-07 13:43:33 +0800

    add 1567

--- Leetcode/DynamicProgramming/1567_medium_MaximumLengthofSubarrayWithPositiveProduct.py
+++ Leetcode/DynamicProgramming/1567_medium_MaximumLengthofSubarrayWithPositiveProduct.py
@@ -38,23 +38,75 @@
 # 链接：https://leetcode-cn.com/problems/maximum-length-of-subarray-with-positive-product
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-class Solution:
+class Solution1:
+    # dp
     def getMaxLen(self, nums) -> int:
         size = len(nums)
-        pos = neg = [0] * size
+        pos = [0] * size
+        neg = [0] * size  # 利用pos和neg来存储每一个下标前的最长连续最大和最小的乘积子数组
         if nums[0] > 0:
             pos[0] = 1
         if nums[0] < 0:
             neg[0] = 1
         res = pos[0]
         for i in range(1, size):
-            if nums[i] > 0:
-                pos[i] = pos[i-1] + 1
+            if nums[i] > 0:  # 若当前下标对应的元素是正数, 当前位为正数，乘上一位对应的最大乘积为仍然是正数，因此当前位正数最大子数组长度位
+                pos[i] = pos[i-1] + 1  # 连续正数+1
+                # 如果neg[i-1]>0，说明i-1的负数最小乘积乘于当前的正数会仍然为负数，而且最小
                 neg[i] = neg[i-1] + 1 if neg[i-1] > 0 else 0
             elif nums[i] < 0:
+                # 当前元素为负数的时候，正数最大乘积的就是负数最小乘积连续长度+1，而负数则为正数最大乘积连续长度+1，即pos[i]和neg[i]的结果要发生交换
+                # 且neg[i-1]需要！=0，因为只有不为0当前的下标才能取到最大乘积值
                 pos[i] = neg[i-1] + 1 if neg[i-1] > 0 else 0
                 neg[i] = pos[i-1] + 1
             else:
                 pos[i] = neg[i] = 0
-            res = max(res, pos[i])
+                # 当前需要进行乘的值如果等于0则正负最长子数组长度均需重新置零重新从下一个数进行遍历
+            res = max(res, pos[i])  # 每次都比较取最大的结果
         return res
+
+
+class Solution2:
+    # 根据dp优化空间
+    def getMaxLen(self, nums) -> int:
+        prepos = 0
+        preneg = 0
+        res = 0
+        for i in range(len(nums)):
+            currpos = 0
+            currneg = 0
+            if nums[i] == 0:
+                currpos = 0
+                currneg = 0
+                prepos, preneg = currpos, currneg
+            elif nums[i] > 0:
+                currpos = prepos + 1
+                currneg = preneg + 1 if preneg != 0 else 0
+                prepos, preneg = currpos, currneg
+            elif nums[i] < 0:
+                currneg = prepos + 1
+                currpos = preneg + 1 if preneg != 0 else 0
+                prepos, preneg = currpos, currneg
+            res = max(res, currpos)
+        return res
+
+
+class Solution:
+    # 二次根据dp优化空间
+    def getMaxLen(self, nums) -> int:
+        prepos, preneg = 0, 0
+        res = 0
+        for n in nums:
+            if n == 0:
+                prepos, preneg = 0, 0
+            elif n > 0:
+                prepos, preneg = prepos + 1, preneg + 1 if preneg != 0 else 0
+            else:
+                preneg, prepos = prepos + 1, preneg + 1 if preneg != 0 else 0
+            res = max(res, prepos)
+        return res
+
+
+nums = [-1, -2, -3, 0, 1]
+nums = [0, 1, -2, -3, -4]
+print(Solution().getMaxLen(nums))
