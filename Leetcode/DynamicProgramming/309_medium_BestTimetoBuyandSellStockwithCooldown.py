dce4b1d16e3c8a809685db33fe99576ac804f28b
AlanConstantine <rlalan@outlook.com>
2022-01-08 15:27:06 +0800

    add 309

--- Leetcode/DynamicProgramming/309_medium_BestTimetoBuyandSellStockwithCooldown.py
+++ Leetcode/DynamicProgramming/309_medium_BestTimetoBuyandSellStockwithCooldown.py
@@ -20,4 +20,24 @@
 
 class Solution:
     def maxProfit(self, prices) -> int:
-        pass
+        if not prices:
+            return 0
+        size = len(prices)
+        # 三个数组表示不同的状态：当天结束后的最大收益
+        keep = [0] * size  # 手上持有股票，说明是前一天就持有的，也可能是前一天没有但当前买入
+        no = [0] * size  # 手上没有持有股票但是不处于冷冻期，即说明前一天没有发生交易，也有可能是因为前一天是冷冻期
+        # 手上没有持有股票且当天处于冷冻期，即说明当天发生了交易，这里的「处于冷冻期」指的是在第i天结束之后的状态。也就是说：如果第i天结束之后处于冷冻期，那么第i+1天无法买入股票。
+        cool = [0] * size
+        keep[0] = -prices[0]
+        for i in range(1, size):
+
+            keep[i] = max(keep[i-1], no[i-1]-prices[i])
+            no[i] = max(no[i-1], cool[i-1])
+            # 我们在第i天结束之后处于冷冻期的原因是在当天卖出了股票，那么说明在第i-1天时我们必须持有一支股票，对应的状态为f[i-1][0]加上卖出股票的正收益prices[i]。因此状态转移方程为：
+            cool[i] = keep[i-1] + prices[i]
+
+        return max(cool[-1], no[-1])
+
+
+prices = [1, 2, 3, 0, 2]
+print(Solution().maxProfit(prices))
