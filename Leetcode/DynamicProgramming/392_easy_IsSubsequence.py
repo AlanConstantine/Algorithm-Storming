3015a8819d3e464ed87c4402730765c795e3e2af
AlanLau <rlalan@outlook.com>
2022-01-21 19:18:24 +0800

    add 392

---
+++ Leetcode/DynamicProgramming/392_easy_IsSubsequence.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# @Author: Alan Lau
+# @Date: 2022-01-21 18:51:56
+
+# 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+# 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+# 进阶：
+
+# 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+
+# 致谢：
+
+# 特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+
+#  
+
+# 示例 1：
+
+# 输入：s = "abc", t = "ahbgdc"
+# 输出：true
+# 示例 2：
+
+# 输入：s = "axc", t = "ahbgdc"
+# 输出：false
+#  
+
+# 提示：
+
+# 0 <= s.length <= 100
+# 0 <= t.length <= 10^4
+# 两个字符串都只由小写字符组成。
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/is-subsequence
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    # 双指针
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        ssize = len(s)
+        tsize = len(t)
+        while i < ssize and j < tsize:
+            if s[i] == t[j]:
+                i += 1  # 如果相等i指向下一个等待j移动
+            j += 1  # 每次都移动,寻找目标值
+        return i == ssize
+
+
+s = "abc"
+t = "ahbgdc"
+print(Solution().isSubsequence(s, t))
